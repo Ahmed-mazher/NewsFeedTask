14ff52dde807884731f972ca660548eab10228b6
Ahmed Mazher <ahmedmazher17@gmail.com>
2022-05-31 15:50:59 +0200

    Initial Commit

---
+++ NewsFeedTask/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  NewsFeedTask
+//
+//  Created by Rivile on 5/31/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
