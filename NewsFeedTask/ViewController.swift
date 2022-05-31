16596544cf617fb44e0ddeb9d7e1340a68649149
Ahmed Mazher <ahmedmazher17@gmail.com>
2022-05-31 16:21:39 +0200

    initial commit and connectivity manager is added

--- NewsFeedTask/ViewController.swift
+++ NewsFeedTask/ViewController.swift
@@ -6,14 +6,39 @@
 //
 
 import UIKit
+import Combine
+
 
 class ViewController: UIViewController {
 
+    var isConnected:Bool = false
+    private var connectivitySubscriber:AnyCancellable?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        setUpConnectivitySubscribers()
     }
 
+    
+    func setUpConnectivitySubscribers() {
+        
+        connectivitySubscriber = ConnectivityMananger.shared().$isConnected.sink(receiveValue: { [weak self](isConnected) in
+            //print(isConnected ? "Online" : "Offline")
+            self?.isConnected = isConnected
+            self?.update()
+        })
+    }
+    
 
+    func update() {
+        if isConnected {
+            //Fetch from network
+            print("network")
+        }else{
+            //Fetch from local
+            print("local")
+        }
+    }
+    
 }
 
